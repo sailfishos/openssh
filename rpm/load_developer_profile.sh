815db1caf44d3dc8bf6a28e84421c95418665383
Ville Nummela <ville.nummela@jolla.com>
2020-11-16 14:10:18 +0200

    [openssh] Try to load developer profile on emulator. JB#51957

--- rpm/load_developer_profile.sh
+++ rpm/load_developer_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Don't load arbitary file from shell-profile directory only when developer profile is enabled.
 # We check for devel-su because that is only installed when developer-mode is installed.
-if [ -e /usr/bin/devel-su ] ; then
+if [ -e /usr/bin/devel-su ] || [ -e /etc/sailfishos-emulator ]; then
     . /etc/profile.d/developer-profile.sh
 fi
